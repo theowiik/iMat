595f1bd3de89232c0169723098727ff2f61c612f
Theo Wiik <theo.le.wiik@gmail.com>
2019-05-05 23:18:03 +0200

    Update BackendController.java

--- src/Controller/BackendController.java
+++ src/Controller/BackendController.java
@@ -6,5 +6,20 @@ import se.chalmers.cse.dat216.project.IMatDataHandler;
  * The controller
  */
 public class BackendController {
-    private static IMatDataHandler db = IMatDataHandler.getInstance();
+    private static IMatDataHandler db;
+
+    /**
+     *
+     */
+    public BackendController() {
+        db = IMatDataHandler.getInstance();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static IMatDataHandler getDb() {
+        return db;
+    }
 }
