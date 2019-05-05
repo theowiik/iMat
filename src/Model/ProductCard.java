163a1c7caccbce26f265e7bed0ff2b0de000a69f
Theo Wiik <theo.le.wiik@gmail.com>
2019-05-05 23:17:33 +0200

    Update ProductCard.java

--- src/Model/ProductCard.java
+++ src/Model/ProductCard.java
@@ -1,6 +1,30 @@
 package Model;
 
-public class ProductCard {
-    private String title;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import se.chalmers.cse.dat216.project.Product;
 
+/**
+ * A "card" including information about a product
+ */
+public class ProductCard extends AnchorPane {
+    @FXML
+    public Text name;
+    @FXML
+    public Text price;
+    @FXML
+    public Text unit;
+
+    ProductCard(Product product) {
+        this.name.setText(product.getName());
+        this.price.setText(String.valueOf(product.getPrice()));
+        this.unit.setText(product.getUnit());
+    }
+
+    ProductCard(String name, String price, String unit) {
+        this.name.setText(name);
+        this.price.setText(price);
+        this.unit.setText(unit);
+    }
 }
