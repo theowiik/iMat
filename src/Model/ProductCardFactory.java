8c5d7f3d8836a881674aa0c76dd2a117e246bd28
Theo Wiik <theo.le.wiik@gmail.com>
2019-05-07 20:48:21 +0200

    Update ProductCardFactory.java

--- src/Model/ProductCardFactory.java
+++ src/Model/ProductCardFactory.java
@@ -7,13 +7,13 @@ import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
 
 public class ProductCardFactory {
+
     /**
-     * Given a product and controller, returns a product card.
+     * Given a product, returns a product card.
      * @param product
      * @return
      */
-    public static ProductCard createProductCard(Product product, iMatController parentController) {
-//        Image image = IMatDataHandler.getInstance().getFXImage(product);
-        return new ProductCard(product, parentController);
+    public static ProductCard createProductCard(Product product) {
+        return new ProductCard(product);
     }
 }
