80523bbd5a634dc0ab7433cd8d45f6f1482aa1e1
Theo Wiik <theo.le.wiik@gmail.com>
2019-05-14 16:28:35 +0200

    product card

--- src/Model/ProductCard.java
+++ src/Model/ProductCard.java
@@ -1,21 +1,29 @@
 package Model;
 
+import Controller.ProductCardController;
 import Controller.iMatController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import se.chalmers.cse.dat216.project.Product;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
 
 /**
  * A "card" includes information about a product
  */
-public class ProductCard extends AnchorPane implements CustomComponent {
+public class ProductCard extends AnchorPane implements CustomComponent, ProductCardObservable {
     @FXML
     public Text name;
     @FXML
@@ -24,9 +32,16 @@ public class ProductCard extends AnchorPane implements CustomComponent {
     public Text unit;
     @FXML
     public ImageView image;
-//    @FXML
-//    public Label amount;
+    @FXML
+    public Label amountLabel;
+    @FXML
+    public Button addButton;
+    @FXML
+    public Button subtractButton;
+
+    List<ProductCardObserver> observers = new ArrayList<>();
     private int amount = 0;
+    private Product product;
 
     /**
      * Creates a new product card.
@@ -35,10 +50,41 @@ public class ProductCard extends AnchorPane implements CustomComponent {
      */
     ProductCard(Product product, Image image) {
         setRoot();
+        this.product = product;
         this.name.setText(product.getName());
         this.price.setText(String.valueOf(product.getPrice()));
         this.unit.setText(product.getUnit());
         this.image.setImage(image);
+        updateAmountLabel();
+        addListenersToButtons();
+    }
+
+    /**
+     * Updates the amount.
+     * @param amount
+     */
+    private void setAmountLabel(int amount) {
+        amountLabel.setText(String.valueOf(amount));
+    }
+
+    // FIXME: 2019-05-14
+    // Uuuuugly. Had problems with adding a controller to it.
+    // Is not planned to be fixed as I dont want to spend more time on this
+    // because its only a prototype.
+    private void addListenersToButtons() {
+        addButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                add();
+            }
+        });
+
+        subtractButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                subtract();
+            }
+        });
     }
 
     @Override
@@ -56,15 +102,52 @@ public class ProductCard extends AnchorPane implements CustomComponent {
 
     public void add() {
         amount++;
-        System.out.println(amount);
+        updateAmountLabel();
+        notifyAllObserversProductAdded(this.product);
+    }
+
+    private void updateAmountLabel() {
+        setAmountLabel(amount);
     }
 
     public void subtract() {
-        amount--;
-        System.out.println(amount);
+        if (amount > 0) {
+            amount--;
+            updateAmountLabel();
+            notifyAllObserversProductRemoved(this.product);
+        }
     }
 
     public int getAmount() {
         return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public void notifyAllObserversProductAdded(Product product) {
+        for (ProductCardObserver productCardObserver : observers) {
+            productCardObserver.productAdded(product);
+            System.out.println("Clicked! (+)");
+        }
+    }
+
+    @Override
+    public void notifyAllObserversProductRemoved(Product product) {
+        for (ProductCardObserver productCardObserver : observers) {
+            productCardObserver.productRemoved(product);
+            System.out.println("Clicked! (-)");
+        }
+    }
+
+    @Override
+    public void addObserver(ProductCardObserver observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeAllObservers() {
     }
 }
