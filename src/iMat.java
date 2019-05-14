1a491aaefa5b686a4e3373b75aefd9db6dc3e259
Theo Wiik <theo.le.wiik@gmail.com>
2019-05-14 16:30:05 +0200

    removed dependencies

--- src/iMat.java
+++ src/iMat.java
@@ -1,8 +1,6 @@
 import Controller.WindowResizeObservable;
 import Controller.WindowResizeObserver;
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
