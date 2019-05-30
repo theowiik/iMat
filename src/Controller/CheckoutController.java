d266658720ff94bcf10db39c7680160983e33441
Br4xen <45483186+Br4xen@users.noreply.github.com>
2019-05-30 12:55:49 +0200

    Vene

--- src/Controller/CheckoutController.java
+++ src/Controller/CheckoutController.java
@@ -73,6 +73,7 @@ public class CheckoutController implements ShoppingCartListener {
     }
 
     private void setDefaultDate() {
+        checkout.d1.setStyle("fx-background-color: #4CAF50;");
         checkout.handled1();
         checkout.confirmDeliveryDate();
         checkout.setDeliveryDateText();
